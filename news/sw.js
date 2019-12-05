62b935e37dc81a7c2cdac2292952fe82931681c6
PAPAUX Hervé <herve.papaux@unifr.ch>
2019-12-05 10:46:27 +0100

    add

---
+++ news/sw.js
@@ -0,0 +1,42 @@
+self.addEventListener('push', function (event) {
+    if (event.data) {
+        console.log('Push succed: ', event.data.text());
+    } else {
+        console.log('This push event has no data.');
+    }
+
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+    self.registration.showNotification(title, options);
+
+});
+const dynamicCacheName = 'site-dynamic-v1';
+// activate event
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
